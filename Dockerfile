4a30d0eb0dff3671bb5277ca422418c9dc887c1e
thecodekitchen <andy@thecodekitchen.xyz>
2023-06-10 14:05:34 -0500

    Added docker-compose

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,10 @@
-# 
-FROM python:3.9
+FROM python:3.10
 WORKDIR /code
-COPY ./requirements.txt /code/requirements.txt
 
-RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
+
+RUN pip install --no-cache-dir fastapi uvicorn
 RUN pip install --no-cache-dir git+https://github.com/thecodekitchen/Bard-API.git
-
+RUN pip install --no-cache-dir surrealdb pickle5
 COPY ./app /code/app
 
-CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "80"]
+CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000"]
--- app/main.py
+++ app/main.py
@@ -28,42 +28,45 @@ app = FastAPI()
 
 @app.post("/ask")
 async def ask_bard(query: Query, authorization: Annotated[str | None, Header()] = None):
-    async with Surreal("ws://surrealdb/rpc") as db:
-        await db.signin({"user": "root", "pass": "root"})
-        await db.use("test", "test")
-        token=authorization
+    db = Surreal("http://surrealdb:8080")
+    await db.connect()
+    await db.signin({"user": "root", "pass": "root"})
+    await db.use("test", "test")
+    token=authorization
 
-        session = requests.Session()
-        session.headers = {
-                    "Host": "bard.google.com",
-                    "X-Same-Domain": "1",
-                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36",
-                    "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
-                    "Origin": "https://bard.google.com",
-                    "Referer": "https://bard.google.com/",
-                }
-        session.cookies.set("__Secure-1PSID", token)
-        existing = {}
-        if query.cid != None:
-            existing = await db.select(f'session:{query.cid}')
-        if existing == {}:
-            bard = Bard(token=token, session=session, timeout=30)
-            if query.ctx != None:
-                load_context(bard, query.ctx)
-            response = bard.get_answer(query.q)
-            cid = response['conversation_id']
-            answer = response['content']
-            hex = pickle.dumps(bard).hex()
-            await db.query(f'CREATE session:{cid} SET bard = "{hex}"')
-            return json.dumps({'answer': answer, 'cid': cid})
-        else:
-            hex = existing['bard']
-            print(hex)
-            print(type(hex))
-            bard = pickle.loads(bytes.fromhex(hex))
-            response = bard.get_answer(query.q)
-            cid = response['conversation_id']
-            answer = response['content']
-            hex = pickle.dumps(bard).hex()
-            await db.query(f'UPDATE session:{cid} SET bard = "{hex}"')
-            return json.dumps({'answer': answer, 'cid': cid})
+    session = requests.Session()
+    session.headers = {
+                "Host": "bard.google.com",
+                "X-Same-Domain": "1",
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36",
+                "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+                "Origin": "https://bard.google.com",
+                "Referer": "https://bard.google.com/",
+            }
+    session.cookies.set("__Secure-1PSID", token)
+    existing = {}
+    if query.cid != None:
+        existing = await db.select(f'session:{query.cid}')
+    if existing == {}:
+        bard = Bard(token=token, session=session, timeout=30)
+        if query.ctx != None:
+            load_context(bard, query.ctx)
+        response = bard.get_answer(query.q)
+        cid = response['conversation_id']
+        answer = response['content']
+        hex = pickle.dumps(bard).hex()
+        await db.query(f'CREATE session:{cid} SET bard = "{hex}"')
+        await db.close()
+        return json.dumps({'answer': answer, 'cid': cid})
+    else:
+        hex = existing['bard']
+        print(hex)
+        print(type(hex))
+        bard = pickle.loads(bytes.fromhex(hex))
+        response = bard.get_answer(query.q)
+        cid = response['conversation_id']
+        answer = response['content']
+        hex = pickle.dumps(bard).hex()
+        await db.query(f'UPDATE session:{cid} SET bard = "{hex}"')
+        await db.close()
+        return json.dumps({'answer': answer, 'cid': cid})
